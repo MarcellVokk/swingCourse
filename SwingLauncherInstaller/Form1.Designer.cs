35afd673b5e786dfffecc3f1e402b23b3b6ae7f1
Gazdag Marcell <gmarcell2007@gmail.com>
2021-10-28 23:11:10 +0200

    'a'

---
+++ SwingLauncherInstaller/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace SwingLauncherInstaller
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.BackColor = System.Drawing.Color.White;
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.ForeColor = System.Drawing.Color.Black;
+			this.textBox1.Location = new System.Drawing.Point(6, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(179, 13);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "D:\\";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.White;
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+			this.button1.ForeColor = System.Drawing.Color.Black;
+			this.button1.Location = new System.Drawing.Point(346, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(28, 25);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "...";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(18, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(61, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Install here:";
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(160)))), ((int)(((byte)(237)))));
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button2.FlatAppearance.BorderSize = 4;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(156, 135);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(81, 32);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Install";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.White;
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Location = new System.Drawing.Point(85, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(259, 25);
+			this.panel1.TabIndex = 4;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.ForeColor = System.Drawing.Color.White;
+			this.checkBox1.Location = new System.Drawing.Point(83, 53);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(139, 17);
+			this.checkBox1.TabIndex = 5;
+			this.checkBox1.Text = "Create desktop shortcut";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.label2.Location = new System.Drawing.Point(185, 6);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(74, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "\\SwingCourse";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+			this.ClientSize = new System.Drawing.Size(392, 179);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(408, 218);
+			this.MinimumSize = new System.Drawing.Size(408, 218);
+			this.Name = "Form1";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Swing Launcher Installer";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ SwingLauncherInstaller/Form1.cs
@@ -0,0 +1,164 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwingLauncherInstaller
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+			{
+				if (fbd.ShowDialog() == DialogResult.OK)
+				{
+					textBox1.Text = fbd.SelectedPath;
+				}
+			}
+		}
+
+		public void Unzip(string path, string to)
+		{
+			ZipFile.ExtractToDirectory(path, to);
+		}
+
+		private static void CopyFilesRecursively(string sourcePath, string targetPath)
+		{
+			//Now Create all of the directories
+			foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+			{
+				Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+			}
+
+			//Copy all the files & Replaces any files with the same name
+			foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+			{
+				System.IO.File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+			}
+		}
+
+		private async void button2_Click(object sender, EventArgs e)
+		{
+			if(textBox1.Text == "")
+			{
+				MessageBox.Show("Please enter a valid path!");
+				return;
+			}
+
+			string appPath = textBox1.Text;
+			string version = "launcher-0.20";
+
+			if(appPath.Last() != '/' && appPath.Last() != '\\')
+			{
+				appPath += @"\";
+			}
+
+			var githubToken = "0";
+
+			var url = "https://github.com/Vadkarika2/swingCourse/archive/" + version + ".zip";
+
+			if (Directory.Exists(textBox1.Text + @"\SwingLauncher"))
+			{
+				return;
+			}
+			else
+			{
+				Directory.CreateDirectory(textBox1.Text + @"\SwingLauncher");
+			}
+
+			var path = textBox1.Text + @"\SwingLauncher\launcher-latest.zip";
+
+			try
+			{
+				using (var client = new System.Net.Http.HttpClient())
+				{
+					var credentials = string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}:", githubToken);
+					credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(credentials));
+					client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", credentials);
+					var contents = await client.GetByteArrayAsync(url);
+					System.IO.File.WriteAllBytes(path, contents);
+				}
+			}
+			catch (Exception)
+			{
+				return;
+			}
+
+			if (!Directory.Exists(textBox1.Text + @"\SwingLauncher\Launcher"))
+			{
+				Directory.CreateDirectory(textBox1.Text + @"\SwingLauncher\Launcher");
+			}
+
+			if (!Directory.Exists(textBox1.Text + @"\SwingLauncher\Launcher"))
+			{
+				Directory.CreateDirectory(textBox1.Text + @"\SwingLauncher\Data\Downloads\LauncherUpdate");
+			}
+
+			await Task.Factory.StartNew(() =>
+			{
+				Unzip(path, textBox1.Text + @"\SwingLauncher\Data\Downloads\LauncherUpdate");
+
+				try
+				{
+					CopyFilesRecursively(textBox1.Text + @"\SwingLauncher\Data\Downloads\LauncherUpdate\swingCourse-" + version, textBox1.Text + @"\SwingLauncher\Launcher");
+				}
+				catch (Exception)
+				{
+
+				}
+			});
+
+			string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+			CreateShortcut("Swing Launcher", appPath + @"\SwingLauncher\Launcher\SwingLauncher.exe", appPath + "SwingLauncher");
+
+			if (checkBox1.Checked)
+			{
+				CreateShortcut("Swing Launcher", appPath + @"\SwingLauncher\Launcher\SwingLauncher.exe", desktop);
+			}
+
+			Process.Start(textBox1.Text + @"\SwingLauncher\Launcher\SwingLauncher.exe");
+			Application.Exit();
+		}
+
+		public void CreateShortcut(string name, string executable, string shortcutPath)
+		{
+			//using (StreamWriter w = new StreamWriter(shortcutPath + "/" + name + ".url"))
+			//{
+			//	w.WriteLine("[InternetShortcut]");
+			//	w.WriteLine("URL=file:///" + link);
+			//	w.WriteLine("IconIndex=0");
+			//	w.Write("IconFile=" + link);
+			//	MessageBox.Show(link);
+			//}
+
+			WshShell shell = new WshShell();
+
+			IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath + @"\" + name + ".lnk");
+			shortcut.TargetPath = executable;
+
+			//shortcut.IconLocation = 'Location of  iCon you want to set";
+
+			// add Description of Short cut
+			shortcut.Description = "Swing Launcher";
+
+			// save it / create
+			shortcut.Save();
+		}
+	}
+}

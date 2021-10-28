9e5cbf74e33b7ab3f7eeb5004cee30f4709b917b
Gazdag Marcell <gmarcell2007@gmail.com>
2021-10-28 22:33:44 +0200

    'a'

---
+++ SwingCourseUploadUpdate/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace SwingCourseUploadUpdate
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button2 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(160)))), ((int)(((byte)(237)))));
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button2.FlatAppearance.BorderSize = 4;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(197, 212);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(91, 34);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Upload";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(15, 61);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(45, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Version:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(294, 233);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(0, 13);
+			this.label3.TabIndex = 5;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.ForeColor = System.Drawing.Color.White;
+			this.checkBox1.Location = new System.Drawing.Point(66, 86);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(71, 17);
+			this.checkBox1.TabIndex = 6;
+			this.checkBox1.Text = "Snapshot";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.ForeColor = System.Drawing.Color.White;
+			this.checkBox2.Location = new System.Drawing.Point(66, 109);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(109, 17);
+			this.checkBox2.TabIndex = 7;
+			this.checkBox2.Text = "Launcher Update";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.White;
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(259, 25);
+			this.panel1.TabIndex = 10;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.BackColor = System.Drawing.Color.White;
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.label1.ForeColor = System.Drawing.Color.Black;
+			this.label1.Location = new System.Drawing.Point(6, 6);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(247, 13);
+			this.label1.TabIndex = 0;
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.White;
+			this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button3.FlatAppearance.BorderSize = 0;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+			this.button3.ForeColor = System.Drawing.Color.Black;
+			this.button3.Location = new System.Drawing.Point(273, 12);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(28, 25);
+			this.button3.TabIndex = 8;
+			this.button3.Text = "...";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.White;
+			this.panel2.Controls.Add(this.textBox1);
+			this.panel2.Location = new System.Drawing.Point(66, 55);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(131, 25);
+			this.panel2.TabIndex = 11;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.BackColor = System.Drawing.Color.White;
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.ForeColor = System.Drawing.Color.Black;
+			this.textBox1.Location = new System.Drawing.Point(6, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(119, 13);
+			this.textBox1.TabIndex = 0;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.ForeColor = System.Drawing.Color.White;
+			this.checkBox3.Location = new System.Drawing.Point(66, 132);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(286, 17);
+			this.checkBox3.TabIndex = 12;
+			this.checkBox3.Text = "Just push to github, no update (version = branch name)";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+			this.ClientSize = new System.Drawing.Size(484, 261);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button2);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximumSize = new System.Drawing.Size(500, 300);
+			this.MinimumSize = new System.Drawing.Size(500, 300);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Swing Launcher Update Managger";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox label1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.CheckBox checkBox3;
+	}
+}
+
---
+++ SwingCourseUploadUpdate/Form1.cs
@@ -0,0 +1,111 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwingCourseUploadUpdate
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		IFirebaseConfig config = new FirebaseConfig
+		{
+			AuthSecret = "UPuTlU8eTdGrm4cHFEkaK8CNceGyx8Ue0EWLpbMD",
+			BasePath = "https://swinglauncher-default-rtdb.firebaseio.com/"
+		};
+
+		IFirebaseClient client;
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			using(FolderBrowserDialog fbd = new FolderBrowserDialog())
+			{
+				if(fbd.ShowDialog() == DialogResult.OK)
+				{
+					label1.Text = fbd.SelectedPath;
+				}
+			}
+		}
+
+		private async void button2_Click(object sender, EventArgs e)
+		{
+			label3.Text = "Uploading update...";
+
+			bool isLauncherUpdate = checkBox2.Checked;
+
+			await Task.Factory.StartNew(() =>
+			{
+				if (checkBox2.Checked)
+				{
+					Process pc = new Process();
+					ProcessStartInfo startInfo = new ProcessStartInfo();
+					startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+					startInfo.Verb = "runas";
+					startInfo.FileName = "cmd.exe";
+					startInfo.WorkingDirectory = label1.Text + @"\";
+					startInfo.Arguments = "/C echo a&cd /d " + label1.Text + "&rmdir /Q /S " + label1.Text + @"\.git&git init&git add .&git branch launcher-" + textBox1.Text + "&git commit -m 'a'&git remote add origin https://github.com/Vadkarika2/swingCourse.git&git push -u origin launcher-" + textBox1.Text + " --force&git init&git add .&git branch launcher-" + textBox1.Text + "&git commit -m 'a'&git remote add origin https://github.com/Vadkarika2/swingCourse.git&git push -u origin launcher-" + textBox1.Text + " --force";
+					pc.StartInfo = startInfo;
+					pc.Start();
+					pc.WaitForExit();
+				}
+				else
+				{
+					Process pc = new Process();
+					ProcessStartInfo startInfo = new ProcessStartInfo();
+					startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+					startInfo.Verb = "runas";
+					startInfo.FileName = "cmd.exe";
+					startInfo.WorkingDirectory = label1.Text + @"\";
+					startInfo.Arguments = "/C echo a&cd /d " + label1.Text + "&rmdir /Q /S " + label1.Text + @"\.git&git init&git add .&git branch " + textBox1.Text + "&git commit -m 'a'&git remote add origin https://github.com/Vadkarika2/swingCourse.git&git push -u origin " + textBox1.Text + " --force&git init&git add .&git branch " + textBox1.Text + "&git commit -m 'a'&git remote add origin https://github.com/Vadkarika2/swingCourse.git&git push -u origin " + textBox1.Text + " --force";
+					pc.StartInfo = startInfo;
+					pc.Start();
+					pc.WaitForExit();
+				}
+			});
+
+			if (!checkBox3.Checked)
+			{
+				if (checkBox2.Checked)
+				{
+					await client.SetAsync("launcher/versions/latest", new Version("launcher-" + textBox1.Text, false));
+				}
+				else
+				{
+					await client.SetAsync("versions/latest", new Version(textBox1.Text, checkBox1.Checked));
+				}
+			}
+
+			label3.Text = "Update uploaded succesfuly!";
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			client = new FirebaseClient(config);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+			{
+				if (fbd.ShowDialog() == DialogResult.OK)
+				{
+					label1.Text = fbd.SelectedPath;
+				}
+			}
+		}
+	}
+}
